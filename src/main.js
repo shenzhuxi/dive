eac0b99f2e266e24ab8bea7bf8de36d382f7b9cd
Liang <shenzhuxi@gmail.com>
2020-08-15 15:49:59 +0100

    Oculus Browser messed up with the requestIdleCallback. Polyfilled main.js to fix it.

--- src/main.js
+++ src/main.js
@@ -13,3 +13,24 @@ startButton.addEventListener( 'click', () => {
 	world.init();
 
 } );
+
+/**
+ * Oculus Browser messed up with the requestIdleCallback. 
+ * https://gist.github.com/paullewis/55efe5d6f05434a96c36
+ */
+
+window.requestIdleCallback = function (cb) {
+    return setTimeout(function () {
+      var start = Date.now();
+      cb({ 
+        didTimeout: false,
+        timeRemaining: function () {
+          return Math.max(0, 50 - (Date.now() - start));
+        }
+      });
+    }, 1);
+  }
+
+window.cancelIdleCallback = function (id) {
+    clearTimeout(id);
+  } 
